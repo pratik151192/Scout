048a48c1ded8558739c99a9dad0234c280165507
pratik151192 <pratikagarwal1@outlook.com>
2016-02-01 05:04:02 +0530

    Create C000R.java

---
+++ C000R.java
@@ -0,0 +1,66 @@
+package net.pocketmagic.android.AndroidBrowser;
+
+/* renamed from: net.pocketmagic.android.AndroidBrowser.R */
+public final class C0000R {
+
+    /* renamed from: net.pocketmagic.android.AndroidBrowser.R.attr */
+    public static final class attr {
+    }
+
+    /* renamed from: net.pocketmagic.android.AndroidBrowser.R.drawable */
+    public static final class drawable {
+        public static final int back = 2130837504;
+        public static final int black = 2130837505;
+        public static final int black1 = 2130837506;
+        public static final int black2 = 2130837507;
+        public static final int icon = 2130837508;
+        public static final int line = 2130837509;
+    }
+
+    /* renamed from: net.pocketmagic.android.AndroidBrowser.R.id */
+    public static final class id {
+        public static final int bluetoothstate = 2131099648;
+        public static final int button1 = 2131099657;
+        public static final int button2 = 2131099658;
+        public static final int button3 = 2131099661;
+        public static final int button4 = 2131099662;
+        public static final int button5 = 2131099663;
+        public static final int confirm = 2131099656;
+        public static final int contact = 2131099652;
+        public static final int editText1 = 2131099660;
+        public static final int exit = 2131099666;
+        public static final int fullscreen = 2131099664;
+        public static final int listpaireddevices = 2131099649;
+        public static final int messenger = 2131099667;
+        public static final int password = 2131099654;
+        public static final int ph = 2131099650;
+        public static final int save = 2131099655;
+        public static final int send = 2131099651;
+        public static final int showtabs = 2131099665;
+        public static final int tableRow1 = 2131099659;
+        public static final int user_name = 2131099653;
+    }
+
+    /* renamed from: net.pocketmagic.android.AndroidBrowser.R.layout */
+    public static final class layout {
+        public static final int activity_main = 2130903040;
+        public static final int bluetoothmain = 2130903041;
+        public static final int homescreen = 2130903042;
+        public static final int main = 2130903043;
+        public static final int phonenumber = 2130903044;
+        public static final int setting = 2130903045;
+        public static final int simple_list_item = 2130903046;
+        public static final int table_layout = 2130903047;
+    }
+
+    /* renamed from: net.pocketmagic.android.AndroidBrowser.R.menu */
+    public static final class menu {
+        public static final int browser_menu = 2131034112;
+    }
+
+    /* renamed from: net.pocketmagic.android.AndroidBrowser.R.string */
+    public static final class string {
+        public static final int app_name = 2130968577;
+        public static final int hello = 2130968576;
+    }
+}
